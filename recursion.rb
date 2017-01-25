895db5eec915aa8767a06f29fdded2502a62009a
Jason Reeder <info@jasonreeder.com>
2017-01-25 07:38:19 -0800

    Add

--- recursion.rb
+++ recursion.rb
@@ -3,3 +3,8 @@ def sum_to(n)
   return nil if n < 1 # Can this be acheived w/o a separe base case?
   n + sum_to(n - 1)
 end
+
+def add_numbers(nums_array)
+  return nums_array.first if nums_array.length <= 1
+  nums_array.first + add_numbers(nums_array[1..-1])
+end
