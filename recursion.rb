08432d498fab23369b524f505f0b309054b3052f
Jason Reeder <info@jasonreeder.com>
2017-01-25 07:49:54 -0800

    Add

--- recursion.rb
+++ recursion.rb
@@ -1,10 +1,16 @@
 def sum_to(n)
   return 1 if n == 1
-  return nil if n < 1 # Can this be acheived w/o a separe base case?
+  return nil if n < 1 # Can this be acheived w/o a separate base case?
   n + sum_to(n - 1)
 end
 
 def add_numbers(nums_array)
   return nums_array.first if nums_array.length <= 1
   nums_array.first + add_numbers(nums_array[1..-1])
+end
+
+def gamma_fnc(n)
+  return 1 if n == 1
+  return nil if n <= 0 # Can this be acheived w/o a separate base case?
+  (n - 1) * gamma_fnc(n - 1)
 end
